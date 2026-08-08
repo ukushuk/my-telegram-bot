7a4bb261ec2f9bd439e6cef430a1d1720fe97e8b
ukushuk <ukushuka314@gmail.com>
2026-08-08 23:13:54 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "I'm alive!"
 
 def run():
-    # Считываем порт от Render
+    # Render автоматически передает нужный порт сюда
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 
